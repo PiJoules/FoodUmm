eb56b5d62b0ea83ed80ed7d7794b9dded9e71920
pijoules <lchan1994@yahoo.com>
2015-09-04 13:58:46 -0400

    just testing the api for searching for restaurants

---
+++ main.py
@@ -0,0 +1,37 @@
+
+# Import the Flask Framework
+from flask import Flask, render_template, jsonify, request, url_for
+app = Flask(__name__)
+
+import private
+from urllib import urlencode
+from urllib2 import Request, urlopen
+import json
+
+@app.route("/")
+def index_route():
+	return render_template("index.html")
+
+@app.route("/test")
+def test_route():
+	payload = {
+		"address": "3141 Chestnut St, Philadelphia, PA 19104",
+		"client_id": private.delivery_client_id
+	}
+	url = "http://sandbox.delivery.com/merchant/search/delivery?" + urlencode(payload)
+	request = Request(url)
+	response = urlopen(request)
+	text = response.read()
+	response.close()
+	return jsonify(response=json.loads(text))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def application_error(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500
